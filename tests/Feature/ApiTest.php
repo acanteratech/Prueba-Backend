c358621381026b45fa6de75f05fd92a82b52072e
acantera@toools.es <acantera@toools.es>
2024-03-06 11:48:48 +0100

    fix

--- tests/Feature/ApiTest.php
+++ tests/Feature/ApiTest.php
@@ -8,11 +8,6 @@ use Tests\TestCase;
 
 class ApiTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
     public function test_short_urls()
     {
         $response = $this->post(route('short-urls'), ["url" => 'http://www.example.com']);
